2f44fc69c8d15146d7ef411f400ce3623640db7d
Rimi Chakraborty <143891192+rimichak@users.noreply.github.com>
2026-03-15 09:55:15 +0000

    Add item numbering to menu display and define select_items function

--- main.py
+++ main.py
@@ -2,9 +2,15 @@
 menus = {"Pizza": 150, "Burger": 200, "Biriyani": 250}
 
 def view():
+    count = 1
     print("*** Today's Menu ***") 
     for item , price in menus.items():
-        print(item, price)
+        print(count, item, price)
+        count += 1
+
+def select_items():
+    pass
+
 
 view()
 
