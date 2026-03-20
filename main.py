f4e70b6c8ca3103a5706fd5401976de09f0e3dc5
Rimi Chakraborty <143891192+rimichak@users.noreply.github.com>
2026-03-20 06:34:11 +0000

    Fix merge conflicts in select_items function and ensure total bill calculation is displayed correctly

--- main.py
+++ main.py
@@ -10,31 +10,21 @@ def view():
 
 def select_items():
    
-<<<<<<< HEAD
    cart = []
-=======
->>>>>>> 59f0cbf015bc6bff4f9f2cf2e803735f9ca7aa0f
    total = 0
    while True:
         selected_item = int(input("Select items from menus to orders: "))
         if selected_item == 1:
-<<<<<<< HEAD
             cart.append(("Pizza", 150))
-=======
->>>>>>> 59f0cbf015bc6bff4f9f2cf2e803735f9ca7aa0f
             print("you selected Pizza" )
             print("your bill is 150 RS")
             total += 150
         if selected_item == 2:
-<<<<<<< HEAD
             cart.append(("Burger", 200))
-=======
->>>>>>> 59f0cbf015bc6bff4f9f2cf2e803735f9ca7aa0f
             print("you selected Burger" )
             print("your bill is 200 RS")
             total += 200
         if selected_item == 3:
-<<<<<<< HEAD
             cart.append(("Biriyani", 250))
             print("you selected Biriyani" )
             print("your bill is 250 RS")
@@ -50,19 +40,9 @@ def select_items():
 
    for item , price in cart:
        print(f"{item} - {price} /-")
+
    print(f"Total bill is: {total} /-")
-
-=======
-            print("you selected Biriyani" )
-            print("your bill is 250 RS")
-            total += 250
-        more = input("Want to add more items? Yes/No: ")
-
-        if more == "No":
-            break
-   print("Total bill is: ", total)
    
->>>>>>> 59f0cbf015bc6bff4f9f2cf2e803735f9ca7aa0f
 
 view()
 select_items()
