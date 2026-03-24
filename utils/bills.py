91833ff3985baa0c2ca36ff1c89e346295ad5170
Rimi Chakraborty <143891192+rimichak@users.noreply.github.com>
2026-03-24 04:49:45 +0000

    Refactor save_bill function for improved formatting and consistency in bill output

--- utils/bills.py
+++ utils/bills.py
@@ -5,15 +5,16 @@ def save_bill(cart, total):
 
     if not os.path.exists(folder):
         os.makedirs(folder)
+
     file_path = os.path.join(folder, "bill.txt")
 
     with open(file_path, "w") as file:
-         file.write("===== FOOD BILL =====\n\n")
+        file.write("===== FOOD BILL =====\n\n")
 
-         for item, price in cart:
-            file.write(f"{item} ₹{price}\n")
+        for item, price in cart:
+            file.write(f"{item:<10} ₹{price}\n")
 
-            file.write("\n----------------------\n")
-    file.write(f"Total: ₹{total}\n")
+        file.write("\n----------------------\n")
+        file.write(f"Total: ₹{total}\n")
 
     print(f"✅ Bill saved in {file_path}")
