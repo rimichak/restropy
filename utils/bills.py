dd4cb448f641b93ed4ab18cfaaf11a382aa799be
Rimi Chakraborty <143891192+rimichak@users.noreply.github.com>
2026-03-24 04:43:59 +0000

    Implement bill saving functionality and update bill format

---
+++ utils/bills.py
@@ -0,0 +1,19 @@
+import os
+
+def save_bill(cart, total):
+    folder = "bills"
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    file_path = os.path.join(folder, "bill.txt")
+
+    with open(file_path, "w") as file:
+         file.write("===== FOOD BILL =====\n\n")
+
+         for item, price in cart:
+            file.write(f"{item} ₹{price}\n")
+
+            file.write("\n----------------------\n")
+            file.write(f"Total: ₹{total}\n")
+
+    print(f"✅ Bill saved in {file_path}")
